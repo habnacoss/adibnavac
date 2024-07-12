c94ff0c8951741c6b1f22b46793050b610bae520
Habib Navarro <habnacos@gmail.com>
2024-07-12 06:04:20 +0000

    better interface

--- src/app/bilingue/2024/page.tsx
+++ src/app/bilingue/2024/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import './styles.css';
+import instagram from './images/instagram.png';
+import youtube from './images/youtube.png';
+import googlephotos from './images/googlephotos.png';
+import Image from 'next/image'
+
 
 export default function Bilingue2024() {
   const searchParams = useSearchParams();
@@ -8,10 +14,37 @@ export default function Bilingue2024() {
   const photos = searchParams.get("photos")?.toString() ?? "";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>{student}</h1>
-      <Link href={photos}>Photos</Link>
-      <Link href="https://www.instagram.com/adibnavac">@adibnavac</Link>
+    <main className="flex flex-col min-h-screen items-center font-sans p-5">
+
+      <div className="flex flex-col max-w-2xl my-auto bg-white opacity-95 rounded-3xl p-10 shadow-2xl">
+        <p className="text-center text-xl">
+          ¡Felicidades a quienes celebran el paso hacia un horizonte lleno de promesas! Que cada día esté lleno de nuevos comienzos y oportunidades emocionantes.
+        </p>
+
+        <hr className="mt-5 mb-10" />
+
+        <Link href={photos} target="_blank" className="text-blue-700 text-lg hover:text-white border border-blue-700 hover:bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+          <Image src={googlephotos} alt="youtube" width={20} height={20} className="inline-block mb-1 mr-2" />
+          Fotos de {student}
+        </Link>
+
+        <Link href={photos} target="_blank" className="text-blue-700 text-lg hover:text-white border border-blue-700 hover:bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+          <Image src={youtube} alt="youtube" width={20} height={20} className="inline-block mb-1 mr-2" />
+          Video de grado
+        </Link>
+
+        <Link href={photos} target="_blank" className="text-blue-700 text-lg hover:text-white border border-blue-700 hover:bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+          <Image src={youtube} alt="youtube" width={20} height={20} className="inline-block mb-1 mr-2" />
+          Video de la fiesta
+        </Link>
+
+        <hr className="mt-5 mb-10" />
+
+        <Link className="mt-auto text-center py-1 px-4 hover:underline hover:text-blue-700" href="https://www.instagram.com/adibnavac" target="_blank">
+          <Image src={instagram} alt="instagram" width={20} height={20} className="inline-block mb-1 mr-2" />
+          @adibnavac
+        </Link>
+      </div>
     </main>
   );
 }
