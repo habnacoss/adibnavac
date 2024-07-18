4781fd56a3bd663e9ed741625ae7fbbce5a3a210
HABNACOS <36807577+habnacos@users.noreply.github.com>
2024-07-18 14:56:58 -0500

    fix: video

--- src/app/bilingue/2024/page.tsx
+++ src/app/bilingue/2024/page.tsx
@@ -23,11 +23,11 @@ export default function Bilingue2024() {
         </Link>
 
         <Link href="https://youtu.be/lU8f3OmKr9Y" target="_blank" className="text-blue-700 text-sm md:text-lg hover:text-white border border-blue-700 hover:bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-          Video de grado
+          Video de la fiesta
         </Link>
 
         <Link href={photos} target="_blank" className="text-blue-700 text-sm md:text-lg hover:text-white border border-blue-700 hover:bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-          Video de la fiesta
+          Video de grado
         </Link>
 
         <hr className="my-3" />
