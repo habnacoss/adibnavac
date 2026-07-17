ab4bb9213325ac40159d64de9765f56e23080511
Habib Navarro Acosta <habib.navarro@analitica.com.co>
2026-07-17 12:33:17 -0500

    fix: update metadata title and description for Bureche 2026 layout

--- src/app/bureche/2026/layout.tsx
+++ src/app/bureche/2026/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "Bilingüe 2026",
-  description: "Bilingüe 2026 - ADIB Navac",
+  title: "Bureche PROMO 2026",
+  description: "Bureche 2026 - ADIB Navac",
 };
 
 export default function RootLayout({
